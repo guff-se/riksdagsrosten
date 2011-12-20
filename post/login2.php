f33d6c9efd9dc72cbb30d45acb91856b7253decc
Guff <guff@guff.se>
2011-12-20 11:12:35 +0100

    ny login test

---
+++ post/login2.php
@@ -0,0 +1,60 @@
+<?php
+
+define('YOUR_APP_ID', '196658153744046');
+define('YOUR_APP_SECRET', '0349d42b3e5992fe738ee45c95248c7f');
+
+function get_facebook_cookie($app_id, $app_secret) {
+  $args = array();
+  parse_str(trim($_COOKIE['fbs_' . $app_id], '\\"'), $args);
+  ksort($args);
+  $payload = '';
+  foreach ($args as $key => $value) {
+    if ($key != 'sig') {
+      $payload .= $key . '=' . $value;
+    }
+  }
+  if (md5($payload . $app_secret) != $args['sig']) {
+    return null;
+  }
+  return $args;
+}
+
+$cookie = get_facebook_cookie(YOUR_APP_ID, YOUR_APP_SECRET);
+
+$user = json_decode(file_get_contents(
+    'https://graph.facebook.com/me?access_token=' .
+    $cookie['access_token']));
+
+?>
+<html>
+  <body>
+    <?php if ($cookie) { ?>
+      Welcome <?= $user->name ?>
+    <?php } else { ?>
+      <fb:login-button></fb:login-button>
+    <?php } ?>
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '<?= YOUR_APP_ID ?>',
+          status     : true, 
+          cookie     : true,
+          xfbml      : true,
+          oauth      : true,
+        });
+
+        FB.Event.subscribe('auth.login', function(response) {
+          window.location.reload();
+        });
+      };
+
+      (function(d){
+         var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+         js = d.createElement('script'); js.id = id; js.async = true;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         d.getElementsByTagName('head')[0].appendChild(js);
+       }(document));
+    </script>
+  </body>
+</html>
