7984b9bf4f7be23f11f11ae4291fd6eea8d49e96
Guff <guff@guff.se>
2012-04-26 18:00:41 +0200

    grejer

---
+++ pages/valkommen.php
@@ -0,0 +1,22 @@
+<?
+$HEADER['title'] = "Om";
+$HEADER['type']="article";
+
+include_once("includes/header.php");
+?>
+<br/>
+<h3 style="margin-left:0px;">Välkommen!</h3>
+
+<p>På riksdagsrosten.se kan du se hur våra riksdagsledamöter har röstat i tidigare omröstningar i riksdagen. Du får se statistik och kommentarer, så att du kan följa upp de frågor du är intresserad av.</p>
+<p>
+Du kan sedan själv gå in och välja vad du skulle röstat i de olika frågorna. Baserat på detta paras du ihop med den riksdagsledamot som röstar mest som dig.
+</p>
+Dessutom kan du rösta i kommande frågor. Inför varje kommande omröstning kommer sedan rösterna ifrån riksdagsrosten.se att sammanställas och skickas till våra riksdagsledamöter, så att de får veta vad just deras väljare verkligen tycker!
+<p>
+<p>
+Vi tycker att riksdagsledamöterna skall representera folkets röst - din röst.
+</p>
+
+<p>Vi som står bakom projektet är  <a href="http://guff.se">Gustaf Josefsson</a> (<a href="http://twitter.com/guff_se">@guff_se</a>), <a href="http://www.joakimgreen.com/">Joakim Green</a> (<a href="http://twitter.com/joakimgreen">@joakimgreen</a>) och <a href="http://labyrint.com">Johan Sandén</a> (<a href="http://twitter.com/sounden">@sounden</a>)</p>
+
+<p><a href="http://www.flickr.com/photos/mfalkvidd/6302763092/">Bild</a></p>
