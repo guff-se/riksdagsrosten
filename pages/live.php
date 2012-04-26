a0cc6b750adc6efac5ce8c1b9e296a3ebbb80601
Guff <guff@guff.se>
2012-04-26 12:06:37 +0200

    prelaunch

---
+++ pages/live.php
@@ -0,0 +1,11 @@
+<?
+$HEADER['title'] = "Om";
+$HEADER['type']="article";
+
+include_once("includes/header.php");
+?>
+<br/>
+<h3 style="margin-left:0px;">Riksdagsrösten Live #rdr24</h3>
+<iframe width="100%" height="400" scrolling="auto" frameborder="0" src="http://static.twingly.com/twingly-live/paused/paused.html?stream=riksdagsrosten" border="0" marginwidth="0" marginheight="0"></iframe>
+
+
---
+++ pages/profil-redigera.php
@@ -0,0 +1,62 @@
+<?php
+
+if(!isset($USER)) {
+    header("Location: /");
+    die(" Prova logga in först");
+    
+    
+}
+
+$result = $db->executeSQL("SELECT count(*) as roster FROM UserRoster WHERE user_id = '$USER->id'", "SELECT");
+$USER->roster=$result->roster;
+
+$roster = $db->executeSQLRows("select Utskottsforslag.dok_id, Utskottsforslag.titel, Voteringar.rubrik, Voteringar.dok_id, UserRoster.* from Utskottsforslag, UserRoster, Voteringar WHERE Utskottsforslag.dok_id=Voteringar.dok_id AND Voteringar.votering_id = UserRoster.votering_id AND UserRoster.user_id = '$USER->id' ORDER BY id DESC LIMIT 8");
+
+
+$ledamoter = $db->executeSQLRows("select * from LedamotMatch, Ledamoter where LedamotMatch.intressent_id = Ledamoter.intressent_id and LedamotMatch.user_id = '$USER->id' order by points desc limit 5");
+
+$result2 = $db->executeSQLRows("select * from PartiMatch where user_id = '$USER->id'");
+foreach($PARTI as $pit => $name) {
+	$partier[$pit]["procent"]="?";
+}
+if(isset($result2))
+	foreach($result2 as $pit) {
+		$partier[$pit->parti]["procent"]=round($pit->procent,2)*100;
+	}
+
+$HEADER['title']="Redigera profil";
+$HEADER['type']="";
+
+include_once("includes/header.php");
+?>
+
+
+<div id="content">
+	<div id="main" class="profil">
+		<h1>Min profil</h1>
+		<a href=/profil/redigera>Redigera</a>
+		<div id="profile-overview" class="box-frame">
+			<div class="inner">
+			<img src="https://graph.facebook.com/<?=$USER->facebook_id; ?>/picture" width="50" height="50" class="left" /><h3 class="left"><?=$USER->tilltalsnamn; ?> <?=$USER->efternamn; ?></h3> <span>(<b><?=$USER->roster; ?></b> röster)</span> <span class="logout"><a href="/logout/">Logga ut</a></span>
+				<div class="clearer">&nbsp;</div>
+            </div>        
+		</div>
+		<br />
+		<div class="box-frame">
+			<div class="inner">
+				Ange det parti du röstade på i senaste riksdagsvalet (2010):
+				<select>
+				<?
+				foreach($PARTI as $p => $p_namn) {
+				?>
+					<option name="<?=$p?>"><?=$p_namn?></option>
+				<? }?>
+				</select>
+				(läs vår integritetspolicy)
+				<br/>
+            </div>        
+		</div>
+		<br />
+
+	</div>
+</div>
