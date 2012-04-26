74513e09637700ce9c301249aaa9dcc6d1f0e9b1
Johan Sandén <sounden@gmail.com>
2012-04-27 01:28:40 +0200

    updated publish_actions

--- post/login.php
+++ post/login.php
@@ -27,7 +27,7 @@
      $_SESSION['state'] = md5(uniqid(rand(), TRUE)); //CSRF protection
      $dialog_url = "https://www.facebook.com/dialog/oauth?client_id=" 
        . $app_id . "&redirect_uri=" . urlencode($my_url) . "&state="
-       . $_SESSION['state'] . "&scope=email,offline_access";
+       . $_SESSION['state'] . "&scope=email,offline_access,publish_actions";
 
      header("Location: $dialog_url");
    }
