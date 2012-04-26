5fcb8f3fcebceb082ec5b4e35105ace00b0f8ce9
Guff <guff@guff.se>
2012-04-26 12:13:12 +0200

    prelaunch

--- pages/live.php
+++ pages/live.php
@@ -5,7 +5,14 @@ $HEADER['type']="article";
 include_once("includes/header.php");
 ?>
 <br/>
-<h3 style="margin-left:0px;">Riksdagsrösten Live #rdr24</h3>
-<iframe width="100%" height="400" scrolling="auto" frameborder="0" src="http://static.twingly.com/twingly-live/paused/paused.html?stream=riksdagsrosten" border="0" marginwidth="0" marginheight="0"></iframe>
-
+<h3 style="margin-left:0px;">Riksdagsrösten Live (#rdr24)</h3>
+<div>
+<div style="width:500px;float:left;">
+<object id="bplayer" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="500" height="910"><embed name="bplayer" src="http://static.bambuser.com/r/player.swf?username=riksdagsrosten" type="application/x-shockwave-flash" width="500" height="910" allowfullscreen="true" allowscriptaccess="always" wmode="opaque"></embed><param name="movie" value="http://static.bambuser.com/r/player.swf?username=riksdagsrosten"></param><param name="allowfullscreen" value="true"></param><param name="allowscriptaccess" value="always"></param><param name="wmode" value="opaque"></param></object>
+</div>
+<div style="width:430px;float:right;">
+<iframe width="430" height="910" scrolling="auto" frameborder="0" src="http://static.twingly.com/twingly-live/paused/paused.html?tabs=false&lang=sv&stream=riksdagsrosten" border="0" marginwidth="0" marginheight="0"></iframe>
+</div>
+<div class="clearer">&nbsp;</div>
+</div>
 
