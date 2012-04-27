7673bdb2aafc4e2b4e9f8571d1d8104abbcda3f2
Guff <guff@guff.se>
2012-04-27 09:05:05 +0200

    new files

---
+++ pages/anvandarvillkor.php
@@ -0,0 +1,72 @@
+<?
+$HEADER['title'] = "Användarvillkor";
+$HEADER['type']="article";
+
+include_once("includes/header.php");
+?>
+<br/>
+<div id="terms" class="box-stroke">
+<h3 style="margin-left:0px;">Användarvillkor</h3>
+
+
+<h5>1. GODKÄNNANDE AV VILLKOR</h5>
+<p>Välkommen! Syftet med Riksdagsrösten är att skapa ett alternativ för kommunikation mellan allmänhet och politiker. Riksdagsröstens syfte är att vara en politiskt neutral plattform och ett trovärdigt verktyg för samhällets engagemang och demokrati. </p>
+<p>Genom att nyttja webbplatsen bekräftar du att du accepterar följande villkor för användningen av innehållet på webbplatsen.</p>
+
+<h5>2. BESKRIVNING AV TJÄNSTEN</h5>
+<p>Riksdagsrösten består av tre funktioner;
+<ol>
+	<li>1. Vi gör det lättare att skapa översikt och hålla reda på vad som händer i riksdagen.</li>
+<li>2. Man kommer att kunna se hur enskilda riksdagsledamöter röstat i olika omröstningar och ta del av statistik och kommentarer. Dessutom erbjuder Riksdagsrösten en särskild funktion där du själv kan gå in och rösta i de olika frågorna och därefter ”matchas” mot riksdagsledamöterna.</li>
+<li>3. Användare kan även rösta i kommande voteringar. Sedan sammanställs skräddarsydda rapporter som skickas ut varje vecka till varje samtliga riksdagsledamöter, som får möjlighet att ta del av folkets (och sina egna väljares) åsikter i de kommande voteringarna.</li>
+</ol>
+</p>
+
+
+<h5>3. TILLÅTEN OCH OTILLÅTEN ANVÄNDNING</h5>
+<p>Besökaren får inte använda dokumentation, mallar eller annat material i en kommersiell verksamhet som rimligen kan anses vara en verksamhet som konkurerar med Riksdagsrösten.</p>
+<p>Besökaren ansvarar också för de handlingar som görs under Besökarens konto.</p>
+<p>Den e-postadress till vilken medlemskapet är kopplat måste vara personligt och ej utställd till en allmän e-postadress hos Besökarens företag. Användarnamn och lösenord (Inloggningsuppgifter) för användning av Riksdagsrösten är personliga och får endast användas av användaren personligen. Besökaren åtar sig att inte avslöja användarnamn och lösenord för medlemskap på Riksdagsrösten till annan part.
+Om Besökaren får kännedom om att inloggningsuppgifterna kommit utomstående till del skall Besökaren omgående se till att byta lösenord.</p>
+
+<h5>4. RÄTTIGHETER</h5>
+<p>Samtliga immateriella rättigheter som omfattar tjänsterna på Riksdagsrösten och de tekniska lösningarna som används för tillhandahållande av dessa, utformning av gränssnitt, programvara, och systemarkitektur och design tillhör Riksdagsrösten och får ej användas av Besökaren i en vidare utsträckning än som följer av funktionerna i tjänsten eller som uttryckligen anges i dessa villkor. Under inga omständigheter förvärvar Besökaren eller tredje man några immaterialrättigheter till tjänsten eller till den programvara eller de tekniska lösningar som används i tjänsten eller till eventuella varumärken som tillhör eller nyttjas av Riksdagsrösten.</p>
+<p>Besökaren förbinder sig att följa gällande upphovsrättsliga regler, andra eventuella relevanta immaterialrättsliga regler, regler om skydd för personuppgifter, marknadsrättslig lagstiftning och annan relevant lagstiftning när Besökaren använder sig av informationen som tillhandahålls via tjänsten Riksdagsrösten.</p>
+<p>Besökaren erhåller genom detta avtal ingen rätt att framställa exemplar av (kopiera) och/eller, överföra, sprida, framföra eller på annat sätt göra tillgängligt för allmänheten eller utomstående, material som tillhandahållits av Riksdagsrösten.</p>
+<p>Besökaren har enbart rätt att till eget medium överföra ingående textmaterial, foton eller bilder för läsning eller utskrift i den utsträckning detta är tillåtet enligt upphovsrättslagen. Elektronisk bearbetning i form av t ex indexering och/eller databaslagring är förbjuden. Besökaren har inte rätt att till annan part förmedla, sälja eller mångfaldiga i informationen ingående textmaterial, foton, andra bilder eller annat verk.</p>
+
+<h5>5. TILLGÄNGLIGHET</h5>
+<p>Riksdagsrösten är normalt tillgänglig via Internet dygnet runt sju dagar i veckan. Tillgängligheten är inte alltid övervakad. Riksdagsrösten kan således ej garantera att Tjänsterna är fria från avbrott, fel eller störningar.</p>
+<p>Riksdagsrösten har rätt att vidta åtgärder som påverkar ovannämnda tillgänglighet, om Riksdagsrösten anser detta vara nödvändigt av tekniska, service-, drifts- eller säkerhetsrelaterade skäl. Besökaren är också medveten om och bekräftar Besökarens möjlighet att bereda sig tillträde till Internet inte kan garanteras av Riksdagsrösten och att Riksdagsrösten inte ansvarar för bristfälligheter i Besökarens egna Internetanslutningar eller utrustning.</p>
+
+<h5>6. RÄTTELSE AV FEL</h5>
+<p>Riksdagsrösten skall åtgärda fel eller brister på ett sätt Riksdagsrösten finner lämpligt. Riksdagsrösten har rätt att stänga webbsajten och tjänsterna på denna för underhållsarbete eller uppgradering.</p>
+
+<h5>7. ÄNDRING AV TJÄNSTERNA PÅ RIKSDAGSRÖSTEN</h5>
+<p>Tjänsterna på Riksdagsrösten kommer löpande att vidareutvecklas. Riksdagsrösten har därför rätt att från tid till annan utföra ändringar i tjänsterna och sådana ändringar kan avse vilka källor som bevakas, antalet bevakade källor, rutinerna för drift, tekniska system eller liknande.</p>
+<p>Riksdagsrösten strävar efter att i möjligaste mån tillse att de uppgifter som finns tillgängliga på webbplatsen är korrekta. Riksdagsrösten lämnar inte någon garanti eller annan utfästelse om uppgifternas innehåll eller dess beskaffenhet i övrigt. Användare av webbplatsen har inte rätt att gentemot Riksdagsrösten göra gällande någon påföljd, inbegripet krav på ersättning för ekonomisk eller annan skada, till följd av fel eller påstått fel i uppgifterna på denna webbplats.</p>
+
+
+<h5>8. AVSTÄNGNING</h5>
+<p>Riksdagsrösten har rätt att med omedelbar verkan stänga av Besökaren från tillgång till Riksdagsrösten (avaktivera Besökarens tillträde till Riksdagsrösten), om
+<ul>
+<li>a) Besökaren använder tjänsten på ett sätt som innebär överträdelse av Riksdagsrösten säkerhets- och administrationsbestämmelser,</li>
+<li>b) Besökaren på annat sätt bryter mot dessa användarvillkor.</li>
+<li>c) Besökaren trots tillsägelse sprider reklam, meddelanden eller kommentarer som kan anses stötande eller mindre passande. Riksdagsrösten förbehåller sig rätten att avgöra vad som anses stötande eller mindre passande.</li>
+</ul>
+<p>Om avstängning sker kan Besökarens konto bli avstängt utan föregående varning.</p>
+
+<h5>9. PERSONUPPGIFTER</h5>
+<p>Enligt personuppgiftslagen (1998:2004) får ingen registrering av personuppgifter ske utan personens medgivande. Genom att bli medlem godkänner du att Riksdagsrösten lagrar de uppgifter du lämnat i syfte att användas för de tjänster Riksdagsrösten och dess samarbetspartners erbjuder.</p>
+<p>Riksdagsrösten behandlar Besökarens personuppgifter för att administrera tjänsterna och prenumerationen, ge Besökaren erbjudanden om nya produkter och tjänster, genomföra andra marknadsaktiviteter, samt för att utgöra underlag för Riksdagsrösten marknads- och kundanalyser, affärs- och metodutveckling och statistiska ändamål. Personuppgifter som Besökaren lämnar till Riksdagsrösten eller som annars registreras i samband med avtalsförhållandet, kan komma att behandlas av Riksdagsrösten såväl i Sverige som utomlands.</p>
+<p>Självklart kan du när som helst avsluta ditt medlemskap och därmed ej erhålla våra utskick.</p>
+<p>Riksdagsrösten kan också inhämta uppgifter från privata och offentliga register och komplettera personuppgifterna med dessa. Personuppgifterna får bevaras och användas för något av ovan angivna ändamål även efter avslutandet av abonnemanget.
+Genom att acceptera dessa villkor lämnar Besökaren sitt samtycke till den behandling av personuppgifter som beskrivits.</p>
+
+<h5>10. TILLÄMPLIG RÄTT OCH TVIST</h5>
+<p>Svensk rätt skall tillämpas på dessa villkor och Parternas avtalsförhållande. Tvister angående tolkning eller tillämpning av dessa villkor och avtalsförhållandet rörande tjänsterna skall avgöras av Stockholms tingsrätt som första instans.</p>
+
+<p>Copyright &copy; 2012 Riksdagsrösten</p>
+
+
+</div>
