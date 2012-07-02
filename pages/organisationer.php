db8b2b536b3ecc66b38b43aebceff37a71b2cb1b
Guff <guff@guff.se>
2012-07-02 16:04:08 +0200

    for organisationer

---
+++ pages/organisationer.php
@@ -0,0 +1,27 @@
+<?
+$HEADER['title'] = "Om";
+$HEADER['type']="article";
+
+include_once("includes/header.php");
+?>
+<br/>
+<div class="box-stroke">
+<h1 style="margin-left:0px;">Riksdagsrösten - ett stätt att bevaka och påverka riksdagen</h1>
+<p>Anmäl dig <a href="https://docs.google.com/a/guff.se/spreadsheet/viewform?formkey=dFB0WTcwRmtJUmd5U2tlT3d4Uk9pY3c6MQ">Riksdagsrösten för Organisationer BETA.</a>
+
+<p>Under hösten kommer Riksdagsrösten att introducera organisationekonton, för de intresseorganisationer, partier, föreningar eller byråer som vill uttrycka sin åsikt i de frågor som våra riksdagsledamöter röstar om. Dessa kommer att fungera som vanliga användarkonton, där man får möjlighet att följa och läsa in sig på de utskottsförslag som går till votering och ge sin röst. När ni röstar med ert organsationskonto kommer er organisations logotyp att dyka upp på voteringssidan och ni får på detta sätt möjlighet att påverka våra användare i den stund de lägger sin röst. Det kommer även vara möjligt att kommentera er ståndpunkt.</p>
+<p>
+	<center>
+<a href="/static/images/org-mockup.png" target="new">
+	<img src="/static/images/org-mockup-small.png"><br>Klicka för att förstora exempel.
+</a>
+</center>
+</p>
+<p>
+	En betaversion av organisationskontot kommer att lanseras i samband med riksdagens öppnande och vi vill nu ge er möjligheten att få vara med från början och påverka utvecklingen av tjänsten. Är du intresserad så <a href="https://docs.google.com/a/guff.se/spreadsheet/viewform?formkey=dFB0WTcwRmtJUmd5U2tlT3d4Uk9pY3c6MQ">anmäl dig till vår beta här.</a>
+<p>
+<p>
+	Under det kommande året kommer ytterligare funktioner för påverkansindustrin, såsom notifiering vid specifika händelser, analysverktyg och api-integrering. För all vår data gäller givetvis vår <a href="http://dev.riksdagsrosten.se/anvandarvillkor">integritetspolicy</a> och all data till partier, media och påverkansindustrin kommer vara anonymiserad och sammanställd.
+</p>
+
+</div>
